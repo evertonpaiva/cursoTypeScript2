7fa2f683df170ad8674cfdce68852021ba9f5f01
Everton Paiva <evertonpaiva@gmail.com>
2019-06-17 16:39:52 -0300

    Estendendo interfaces

--- app/ts/models/index.ts
+++ app/ts/models/index.ts
@@ -2,4 +2,7 @@ export * from './Negociacoes';
 export * from './Negociacao';
 export * from './NegociacaoParcial';
 export * from './Imprimivel';
+export * from './Igualavel';
+export * from './MeuObjeto';
+
 
