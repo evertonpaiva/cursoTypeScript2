7a3ca4b604113b3f81e9af3c71c0bb02b4d22b99
Everton Paiva <evertonpaiva@gmail.com>
2019-06-17 09:39:31 -0300

    Removendo comentários em produção.

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,4 @@
 const controller = new NegociacaoController();
-
-$('.form').submit(controller.adiciona.bind(controller));
+document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
