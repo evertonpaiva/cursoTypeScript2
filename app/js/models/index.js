a81d486b034427cdc9411d385370f100a3756453
Everton Paiva <evertonpaiva@gmail.com>
2019-06-17 15:49:34 -0300

    Alterando de herança pra interface do método

--- app/js/models/index.js
+++ app/js/models/index.js
@@ -1,4 +1,4 @@
-System.register(["./Negociacoes", "./Negociacao", "./Imprimivel"], function (exports_1, context_1) {
+System.register(["./Negociacoes", "./Negociacao"], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     function exportStar_1(m) {
@@ -15,9 +15,6 @@ System.register(["./Negociacoes", "./Negociacao", "./Imprimivel"], function (exp
             },
             function (Negociacao_1_1) {
                 exportStar_1(Negociacao_1_1);
-            },
-            function (Imprimivel_1_1) {
-                exportStar_1(Imprimivel_1_1);
             }
         ],
         execute: function () {
